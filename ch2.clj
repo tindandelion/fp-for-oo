e393b42991fc4352e5844a2b18b3d6976ea6f7cb
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-03-24 12:17:23 +0400

    Classes are separate from instances

--- ch2.clj
+++ ch2.clj
@@ -2,37 +2,45 @@
 (ns org.dandelion.fp-for-oo
   (:use clojure.test))
 
+(defn send-to [object message & args]
+  (apply (message (:__instance_methods__ object)) object args))
+
 (defn make [klass & args]
-  (apply klass args))
+  (let [allocated {}
+        initialized (merge allocated klass)]
+    (apply send-to initialized :add-instance-values args)))
 
 (def class-of :__class_symbol__)
 
-(defn send-to [object message & args]
-  (apply (message (:__methods__ object)) object args))
-
-(defn Point [x y]
-  {:x x,
-   :y y
-   :__class_symbol__ 'Point,
-   :__methods__
+(def Point
+  {
+   :__class_symbol__ 'Point
+   :__instance_methods__
    {
     :x (fn [this] (:x this))
     :y (fn [this] (:y this))
+    :add-instance-values (fn [this x y]
+                           (assoc this :x x :y y))
     :shift (fn [this incx incy]
-             (Point (+ (send-to this :x) incx)
-                    (+ (send-to this :y) incy)))
+             (make Point
+                   (+ (send-to this :x) incx)
+                   (+ (send-to this :y) incy)))
     :add (fn [this that]
            (send-to this :shift
                     (send-to that :x)
                     (send-to that :y)))
-    
-    }})
-           
-(defn Triangle [p1 p2 p3]
-  {:point1 p1,
-   :point2 p2,
-   :point3 p3,
-   :__class_symbol__ 'Triangle})
+    }
+   })
+
+(def Triangle
+  {
+   :__class_symbol__ 'Triangle
+   :__instance_methods__
+   {
+    :add-instance-values (fn [this pt1 pt2 pt3]
+                           (assoc this :point1 pt1 :point2 pt2 :point3 pt3))
+    }
+   })
 
 (defn equal-triangles? [& triangles]
   (apply = triangles))
@@ -40,17 +48,20 @@
 (defn valid-triangle? [& points]
   (= (distinct points) points))
 
-(def right-triangle (Triangle (Point 0 0)
-                              (Point 0 1)
-                              (Point 1 0)))
+(def right-triangle (make Triangle
+                          (make Point 0 0)
+                          (make Point 0 1)
+                          (make Point 1 0)))
 
-(def equal-right-triangle (Triangle (Point 0 0)
-                                    (Point 0 1)
-                                    (Point 1 0)))
+(def equal-right-triangle (make Triangle
+                                (make Point 0 0)
+                                (make Point 0 1)
+                                (make Point 1 0)))
 
-(def different-triangle (Triangle (Point 0 0)
-                                  (Point 0 10)
-                                  (Point 10 0)))
+(def different-triangle (make Triangle
+                              (make Point 0 0)
+                              (make Point 0 10)
+                              (make Point 10 0)))
 
 
 ;; ---- Tests
@@ -72,26 +83,27 @@
     (is (= 8 (send-to added-p :x)))
     (is (= 10 (send-to added-p :y)))))
 
-;; (deftest TriangleTest
-;;   (def t (make Triangle
-;;                (make Point 0 0)
-;;                (make Point 1 0)
-;;                (make Point 0 1)))
-;;   (testing "creation"
-;;     (is (= 'Triangle (class-of t))))
-;;   (testing "equality"
-;;     (is (equal-triangles? right-triangle right-triangle))
-;;     (is (not (equal-triangles? right-triangle different-triangle)))
-;;     (is (equal-triangles? right-triangle equal-right-triangle))
-;;     (is (not (equal-triangles? right-triangle
-;;                                equal-right-triangle
-;;                                different-triangle))))
-;;   (testing "validity"
-;;     (is (valid-triangle? (make Point 0 0)
-;;                          (make Point 1 0)
-;;                          (make Point 0 1)))
-;;     (is (not (valid-triangle? (make Point 0 0)
-;;                               (make Point 0 0)
-;;                               (make Point 0 1))))))
+
+(deftest TriangleTest
+  (def t (make Triangle
+               (make Point 0 0)
+               (make Point 1 0)
+               (make Point 0 1)))
+  (testing "creation"
+    (is (= 'Triangle (class-of t))))
+  (testing "equality"
+    (is (equal-triangles? right-triangle right-triangle))
+    (is (not (equal-triangles? right-triangle different-triangle)))
+    (is (equal-triangles? right-triangle equal-right-triangle))
+    (is (not (equal-triangles? right-triangle
+                               equal-right-triangle
+                               different-triangle))))
+  (testing "validity"
+    (is (valid-triangle? (make Point 0 0)
+                         (make Point 1 0)
+                         (make Point 0 1)))
+    (is (not (valid-triangle? (make Point 0 0)
+                              (make Point 0 0)
+                              (make Point 0 1))))))
 
 (run-tests)

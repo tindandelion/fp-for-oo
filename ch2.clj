b14ef95925f86ca29e745f83ec17ffee3ba32755
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-03-24 09:00:09 +0400

    Triangle and its operations

--- ch2.clj
+++ ch2.clj
@@ -22,6 +22,30 @@
 (defn add [p1 p2]
   (shift p1 (x p2) (y p2)))
 
+(defn Triangle [p1 p2 p3]
+  {:point1 p1,
+   :point2 p2,
+   :point3 p3,
+   :__class_symbol__ 'Triangle})
+
+(defn equal-triangles? [& triangles]
+  (apply = triangles))
+
+(defn valid-triangle? [& points]
+  (= (distinct points) points))
+
+(def right-triangle (Triangle (Point 0 0)
+                              (Point 0 1)
+                              (Point 1 0)))
+
+(def equal-right-triangle (Triangle (Point 0 0)
+                                    (Point 0 1)
+                                    (Point 1 0)))
+
+(def different-triangle (Triangle (Point 0 0)
+                                  (Point 0 10)
+                                  (Point 10 0)))
+
 
 ;; ---- Tests
 
@@ -40,5 +64,27 @@
     (def added-p (add p (Point 5 6)))
     (is (= 8 (x added-p)))
     (is (= 10 (y added-p)))))
+
+(deftest TriangleTest
+  (def t (make Triangle
+               (make Point 0 0)
+               (make Point 1 0)
+               (make Point 0 1)))
+  (testing "creation"
+    (is (= 'Triangle (class-of t))))
+  (testing "equality"
+    (is (equal-triangles? right-triangle right-triangle))
+    (is (not (equal-triangles? right-triangle different-triangle)))
+    (is (equal-triangles? right-triangle equal-right-triangle))
+    (is (not (equal-triangles? right-triangle
+                               equal-right-triangle
+                               different-triangle))))
+  (testing "validity"
+    (is (valid-triangle? (make Point 0 0)
+                         (make Point 1 0)
+                         (make Point 0 1)))
+    (is (not (valid-triangle? (make Point 0 0)
+                              (make Point 0 0)
+                              (make Point 0 1))))))
 
 (run-tests)
